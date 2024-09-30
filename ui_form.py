1fe34369f6d76d4ae3bc05feda9f275bb06ffcac
Joana Catarino <joana.fa.catarino@gmail.com>
2024-09-30 11:53:17 +0200

    new gui test

--- ui_form.py
+++ ui_form.py
@@ -15,16 +15,41 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QPushButton, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QLabel,
+    QLineEdit, QPushButton, QRadioButton, QSizePolicy,
+    QWidget)
 
 class Ui_Widget(object):
     def setupUi(self, Widget):
         if not Widget.objectName():
             Widget.setObjectName(u"Widget")
-        Widget.resize(479, 260)
+        Widget.resize(746, 617)
         self.pushButton = QPushButton(Widget)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(100, 100, 241, 24))
+        self.pushButton.setGeometry(QRect(10, 40, 241, 24))
+        self.label = QLabel(Widget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(20, 90, 31, 16))
+        self.lineEdit = QLineEdit(Widget)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(60, 90, 131, 22))
+        self.radioButton = QRadioButton(Widget)
+        self.radioButton.setObjectName(u"radioButton")
+        self.radioButton.setGeometry(QRect(30, 150, 89, 20))
+        self.checkBox = QCheckBox(Widget)
+        self.checkBox.setObjectName(u"checkBox")
+        self.checkBox.setGeometry(QRect(30, 180, 76, 20))
+        self.comboBox = QComboBox(Widget)
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox.setGeometry(QRect(340, 40, 191, 22))
+        self.widget = QWidget(Widget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(330, 110, 341, 251))
+        self.widget.setStyleSheet(u"\n"
+"background-color: rgb(189, 189, 189);")
 
         self.retranslateUi(Widget)
 
@@ -34,5 +59,12 @@ class Ui_Widget(object):
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
         self.pushButton.setText(QCoreApplication.translate("Widget", u"Press me !!", None))
+        self.label.setText(QCoreApplication.translate("Widget", u"Text", None))
+        self.radioButton.setText(QCoreApplication.translate("Widget", u"Sound ", None))
+        self.checkBox.setText(QCoreApplication.translate("Widget", u" light", None))
+        self.comboBox.setItemText(0, QCoreApplication.translate("Widget", u"option 1", None))
+        self.comboBox.setItemText(1, QCoreApplication.translate("Widget", u"option 2", None))
+        self.comboBox.setItemText(2, QCoreApplication.translate("Widget", u"option 3", None))
+
     # retranslateUi
 
